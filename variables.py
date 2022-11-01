0dc511fda9d57e5b812f71edcb82ed03d92a03e6
Thomas van Boom <38430385+CS-TvB@users.noreply.github.com>
2022-11-01 09:55:54 +0100

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,19 @@
+# Do not modify these lines
+__winc_id__ = '7152c06aa3ac4d5f964ca8619ecb7e8f'
+__human_name__ = 'variables'
+
+# Add your code after this line
+from xmlrpc.client import boolean
+
+jaartal = 'i am walking this earth for how manny years now?,'
+oud = 25
+print(jaartal, oud)
+
+a = True
+b = False
+print(type(a))
+
+if a == True:
+    print('noice')
+if a == False:
+    print('not so noice')
